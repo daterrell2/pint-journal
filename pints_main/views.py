e2e06d414ddcafccf4add5ee6002b67fae14c545
D. Terrell <daterrell2@gmail.com>
2015-06-13 18:04:35 -0400

    Views for single beer and single brewery. Added some convenience methodes for Beer and Brewery models

--- pints_main/views.py
+++ pints_main/views.py
@@ -12,3 +12,32 @@ def main_page(request):
 	beer_list = Beer.objects.order_by('-date_added')[:5]
 	context_dict = {'breweries': brewery_list, 'beers': beer_list}
 	return render(request, 'pints_main/main_page.html', context_dict)
+
+def beer_detail(request, beer_name_slug):
+	context_dict = {}
+
+	try:
+		beer = Beer.objects.get(slug = beer_name_slug)
+		context_dict['beer'] = beer
+		context_dict['brewery'] = beer.brewery
+		beer_score = beer.get_top_score()
+		context_dict['beer_score'] = beer_score
+
+	except beer.DoesNotExist:
+		return redirect('/')
+
+	return render(request, 'pints_main/beer_detail.html', context_dict)
+
+def brewery_detail(request, brewery_name_slug):
+	context_dict = {}
+
+	try:
+		brewery = Brewery.objects.get(slug = brewery_name_slug)
+		context_dict['brewery'] = brewery
+		beers = brewery.get_beers()
+		context_dict['beers'] = beers
+
+	except Brewery.DoesNotExist:
+		return redirect('/')
+
+	return render(request, 'pints_main/brewery_detail.html', context_dict)

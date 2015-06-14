5bc72a500ff63b4b21ec29f56bd989dbc62a8d8a
D. Terrell <daterrell2@gmail.com>
2015-06-14 02:41:43 -0400

    Added views and templates to allow editing and deleting beers

--- pints_main/urls.py
+++ pints_main/urls.py
@@ -5,5 +5,7 @@ urlpatterns = patterns('',
 	url(r'^$', views.main_page, name='main_page'),
 	url(r'^brewery/(?P<brewery_name_slug>[\w\-]+)/?$', views.brewery_detail, name = 'brewery_detail'),
 	url(r'^brewery/(?P<brewery_name_slug>[\w\-]+)/add_beer/?$', views.add_beer, name = 'add_beer'),
+	url(r'^beer/(?P<beer_name_slug>[\w\-]+)/edit_beer/?$', views.edit_beer, name = 'edit_beer'),
+	url(r'^beer/(?P<beer_name_slug>[\w\-]+)/delete_beer/?$', views.delete_beer, name = 'delete_beer'),
 	url(r'^beer/(?P<beer_name_slug>[\w\-]+)/?$', views.beer_detail, name = 'beer_detail'),
 	)

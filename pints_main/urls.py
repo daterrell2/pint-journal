8525540f775623488bdfe7cc49113d8dc9223b63
D. Terrell <daterrell2@gmail.com>
2015-06-14 00:29:17 -0400

    Added get_absolute_url methods for brewery and beer models, updated in templates accordingly

--- pints_main/urls.py
+++ pints_main/urls.py
@@ -3,7 +3,7 @@ from pints_main import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.main_page, name='main_page'),
-	url(r'^brewery/(?P<brewery_name_slug>[\w\-]+)[\-\d\/]?$', views.brewery_detail, name = 'brewery_detail'),
-	url(r'^brewery/(?P<brewery_name_slug>[\w\-]+)[\-\d]?/add_beer/?$', views.add_beer, name = 'add_beer'),
-	url(r'^beer/(?P<beer_name_slug>[\w\-%]+)[\-\d\/]?$', views.beer_detail, name = 'beer_detail'),
+	url(r'^brewery/(?P<brewery_name_slug>[\w\-]+)/?$', views.brewery_detail, name = 'brewery_detail'),
+	url(r'^brewery/(?P<brewery_name_slug>[\w\-]+)/add_beer/?$', views.add_beer, name = 'add_beer'),
+	url(r'^beer/(?P<beer_name_slug>[\w\-]+)/?$', views.beer_detail, name = 'beer_detail'),
 	)

89a1f2651c9ecf5872f82eaddc6251402369249c
D. Terrell <daterrell2@gmail.com>
2015-06-29 23:36:31 -0400

    reset db migrations for pints_main; rearranged db structure around BreweryDB api; recreated index page with api calls in place of db calls

--- pints_main/forms.py
+++ pints_main/forms.py
@@ -1,37 +1,37 @@
 from django import forms
-from pints_main.models import  Brewery, Beer, BeerScore
+from pints_main.models import BeerScore
 
-class BreweryForm(forms.ModelForm):
-	name = forms.CharField(max_length=128, help_text="Brewery Name")
-	country = forms.CharField(max_length=128, help_text="Country")
-	brew_type = forms.CharField(max_length=128, help_text="Brewery Type")
-	url = forms.URLField(max_length=200, help_text="url")
-	slug = forms.SlugField(widget=forms.HiddenInput())
-	date_added = forms.DateTimeField(widget=forms.HiddenInput())
-	date_modified = forms.DateTimeField(widget=forms.HiddenInput())
+# class BreweryForm(forms.ModelForm):
+# 	name = forms.CharField(max_length=128, help_text="Brewery Name")
+# 	country = forms.CharField(max_length=128, help_text="Country")
+# 	brew_type = forms.CharField(max_length=128, help_text="Brewery Type")
+# 	url = forms.URLField(max_length=200, help_text="url")
+# 	slug = forms.SlugField(widget=forms.HiddenInput())
+# 	date_added = forms.DateTimeField(widget=forms.HiddenInput())
+# 	date_modified = forms.DateTimeField(widget=forms.HiddenInput())
 
-	class Meta:
-		model = Brewery
-		fields = ('name', 'country', 'brew_type', 'url')
+# 	class Meta:
+# 		model = Brewery
+# 		fields = ('name', 'country', 'brew_type', 'url')
 
-	def clean(self):
-		cleaned_data = self.cleaned_data
-		url = cleaned_data.get('url')
+# 	def clean(self):
+# 		cleaned_data = self.cleaned_data
+# 		url = cleaned_data.get('url')
 
-		if url and not url.startswith('http://'):
-			url = 'http://' + url
-			cleaned_data['url'] = url
+# 		if url and not url.startswith('http://'):
+# 			url = 'http://' + url
+# 			cleaned_data['url'] = url
 
-		return cleaned_data
+# 		return cleaned_data
 
-class BeerForm(forms.ModelForm):
-	name = forms.CharField(max_length=128, help_text="Beer Name")
-	beer_style = forms.CharField(max_length=128, help_text="Style")
-	slug = forms.SlugField(widget=forms.HiddenInput(), required=False)
+# class BeerForm(forms.ModelForm):
+# 	name = forms.CharField(max_length=128, help_text="Beer Name")
+# 	beer_style = forms.CharField(max_length=128, help_text="Style")
+# 	slug = forms.SlugField(widget=forms.HiddenInput(), required=False)
 
-	class Meta:
-		model = Beer
-		fields = ('name', 'beer_style')
+# 	class Meta:
+# 		model = Beer
+# 		fields = ('name', 'beer_style')
 
 class BeerScoreForm(forms.ModelForm):
 	score = forms.IntegerField(help_text="Score")
--- populate_db.py
+++ populate_db.py
@@ -4,72 +4,19 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pint_journal_project.settings')
 import django
 django.setup()
 
-from pints_main.models import Brewery, Beer, BeerScore
+from pints_main.models import BeerScore
+from pints_main.utils.brewerydb import *
 from django.contrib.auth.models import User
 
 user = User.objects.get(username=u'david')
 
 def populate():
+
+	beers = ['zGBmz4', 'kZsjqY', '9KiCpK', 'Kax7jD', 'nEEnk1', 'kckAgC']
 	BeerScore.objects.all().delete()
-	Beer.objects.all().delete()
-	Brewery.objects.all().delete()
+	for i in range(len(beers)):
+		add_score(beers[i], 80+i)
 
-	brew_1 = add_brew('Souther Tier', 'United States', 'Micro', '',)
-	brew_2 = add_brew('Port City', 'United States', 'Micro', '')
-	brew_3 = add_brew('DC Brau', 'United States', 'Micro', '')
-
-	beer_1 = add_beer(brewery=brew_1,
-		name = 'Creme Brulee',
-		beer_style = 'Milk Stout')
-
-	beer_2 = add_beer(brewery=brew_1,
-		name = '2x IPA',
-		beer_style = 'American IPA')
-
-	beer_3 = add_beer(brewery=brew_2,
-		name = 'Porter',
-		beer_style = 'American Porter')
-
-	beer_4 = add_beer(brewery=brew_2,
-		name = 'Optimal Wit',
-		beer_style = 'Witbier')
-
-	beer_5 = add_beer(brewery=brew_3,
-		name='The Corruption',
-		beer_style='American IPA')
-
-	beer_6 = add_beer(brewery=brew_3,
-		name='The Public Ale',
-		beer_style='American Pale Ale')
-
-	add_score(beer = beer_1, score = 95)
-
-	add_score(beer = beer_2, score = 85)
-
-	add_score(beer = beer_3, score = 80)
-
-	add_score(beer = beer_4, score = 90)
-
-	add_score(beer = beer_5, score = 85)
-
-	add_score(beer = beer_6, score = 90)
-
-def add_brew(name, country, brew_type, url):
-	b = Brewery()
-	b.name = name
-	b.country = country
-	b.brew_type = brew_type
-	b.url = url
-	b.save()
-	return b
-
-def add_beer(brewery, name, beer_style):
-	br = Beer()
-	br.brewery = brewery
-	br.name = name
-	br.beer_style = beer_style
-	br.save()
-	return br
 
 def add_score(beer, score, user=user):
 	s = BeerScore()

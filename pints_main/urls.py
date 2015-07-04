41de781dd9d560b04d28ebc2c3520a94f8009a2f
D. Terrell <daterrell2@gmail.com>
2015-07-04 00:11:42 -0400

    added edit parameter to beer_detail view

--- pints_main/urls.py
+++ pints_main/urls.py
@@ -4,6 +4,7 @@ from pints_main import views
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
 	url(r'^beer/(?P<beer_id>[\w\-]+)/?$', views.beer_detail, name = 'beer_detail'),
+	url(r'^beer/(?P<beer_id>[\w\-]+)/add_score/?$', views.beer_detail, name = 'add_score'),
 	url(r'^brewery/(?P<brewery_id>[\w\-]+)/?$', views.brewery_detail, name = 'brewery_detail'),
 	# url(r'^brewery/add_beer/(?P<brewery_name_slug>[\w\-]+)/?$', views.add_beer, name = 'add_beer'),
 	# url(r'^beer/edit_beer/(?P<beer_name_slug>[\w\-]+)/?$', views.edit_beer, name = 'edit_beer'),

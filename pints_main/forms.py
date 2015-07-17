05755e822bb5cdb9d90348ef341aa6530b70c92a
D. Terrell <daterrell2@gmail.com>
2015-07-17 02:41:33 -0400

    Started AJAX requests for beer scores. Not Complete!

--- pints_main/forms.py
+++ pints_main/forms.py
@@ -2,11 +2,16 @@ from django import forms
 from pints_main.models import BeerScore
 
 class BeerScoreForm(forms.ModelForm):
-	score = forms.IntegerField(help_text="Score", label="Score", initial="100")
+	#score = forms.IntegerField(help_text="Score", label="Score", initial="100")
 
 	class Meta:
 		model = BeerScore
 		fields = ('score',)
+		widgets = {
+		'score' : forms.NumberInput(
+			attrs={'id':'score-val', 'required': True, 'placeholder':'score'}
+			),
+		}
 		
 
 

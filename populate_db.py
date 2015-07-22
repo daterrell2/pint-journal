30086a3c68f0689e758c56ec59af778289e6277a
daterrell <daterrell2@gmail.com>
2015-07-22 17:56:48 +0000

    Changes to population script

--- populate_db.py
+++ populate_db.py
@@ -5,36 +5,40 @@ import django
 django.setup()
 
 from pints_main.models import BeerScore, Beer
-from pints_main.utils.brewerydb import *
+#from pints_main.utils.brewerydb import *
 from django.contrib.auth.models import User
-
-users = [User.objects.get(username=u) for u in [u'david', u'david_test', u'new']]
+import random
+from pints_main.utils.random_string import random_string
 
 def populate():
 
-	beer_ids = ['zGBmz4', 'kZsjqY', '9KiCpK', 'Kax7jD', 'nEEnk1', 'kckAgC']
-	BeerScore.objects.all().delete()
-	Beer.objects.all().delete()
+    users = [add_user('tmp_user' + str(i)) for i in range(10)]
 
-	beers = [add_beer(i) for i in beer_ids]
+    beer_ids = ['zGBmz4', 'kZsjqY', '9KiCpK', 'Kax7jD', 'nEEnk1', 'kckAgC']
 
-	for i in range(len(beers)):
-		for n in range(len(users)):
-			add_score(beers[i], 80+i+n, users[n])
+    beers = [add_beer(i) for i in beer_ids]
+
+    for b in beers:
+		for u in users:
+			add_score(b, random.randint(0,100), u)
 
 def add_beer(beer_id):
-	b = Beer(beer_id = beer_id)
-	b.save()
+	b, created = Beer.objects.get_or_create(beer_id = beer_id)
 	return b
 
 
 def add_score(beer, score, user):
-	s = BeerScore()
-	s.beer = beer
-	s.user = user
+	s, created = BeerScore.objects.get_or_create(beer=beer, user=user)
 	s.score = score
 	s.save()
 	return s
+
+def add_user(username):
+    u, created = User.objects.get_or_create(username = username)
+    if created:
+        u.set_password(random_string())
+        u.save()
+    return u
 
 if __name__ == '__main__':
 	print "Starting db population script"
